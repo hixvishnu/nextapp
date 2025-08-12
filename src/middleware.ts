aaa4a62b7c19831c98962c8c7fa8a3f806655063
hixvishnu <hixvishnu@gmail.com>
2025-08-12 10:17:13 +0530

    add middleware and me route

---
+++ src/middleware.ts
@@ -0,0 +1,22 @@
+import { NextResponse } from "next/server"
+import type { NextRequest } from "next/server"
+
+export function middleware(request: NextRequest) {
+  const path = request.nextUrl.pathname
+
+  const isPulicPath = path === "/login" || path === "/signup"
+
+  const token = request.cookies.get("token")?.value || ""
+
+  if (isPulicPath && token) {
+    return NextResponse.redirect(new URL("/", request.nextUrl))
+  }
+
+  if (!isPulicPath && !token) {
+    return NextResponse.redirect(new URL("/login", request.nextUrl))
+  }
+}
+
+export const config = {
+  matcher: ["/", "/profile", "/login", "/signup"],
+}
